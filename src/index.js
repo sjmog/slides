8ccee895ae07bef964f91e6b2edbb76124625a3f
Sam Morgan <s_morgan@me.com>
2017-05-09 14:56:48 +0100

    Some spacing, and an Alexa-specific talk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 require('smoothscroll-polyfill').polyfill()
 
-const LINES = [
-  ["Hello"],
-  ["Welcome to Makers Academy"],
-  ["It's", "Great", "Here"],
-  ["It's", "Great", "Here"],["It's", "Great", "Here"],
-  ["Welcome to Makers Academy"],
-  ["It's", "Great", "Here"],
-  ["Great to see you!"],
-  ["Welcome to Makers Academy"],
-  ["Welcome to Makers Academy"],
-  ["Hello"],
-  ["Great to see you!"]
+const LINE_DEFINITIONS = [
+  ["Alexa"],
+  ["Is a lot of fun"],
+  ["Ruby"],
+  ["AWS Lambda"],
+  ["HTTPS Endpoint"],
+  ["Pain in the ass"],
+  [["Production"], ["Development"]],
+  ["Ruby ♥ little web projects"],
+  [["Rack"], ["(Sinatra)"]],
+  [["Readable DSL"], ["Easy Routing"], ["Flexible"]],
+  ["Alexa + Sinatra"],
+  [["ngrok"], ["HTTPS"], ["Custom domain"]],
+  ["JSON"],
+  [["Disgusting hash conversions"], ["Painful testing"]],
+  ["Disgusting"],
+  [["Ruby + Alexa framework?"], ["(for Sinatra)"]],
+  ["Ralyxa"],
+  [["Nice interface"], ["Sensible defaults"], ["Sinatra-esque"]],
+  ["ask and tell syntax"],
+  ["Separate routing from Intent handling"],
+  ["Fun"]
 ]
 
 const mountSegment = segment => {
@@ -22,35 +32,37 @@ const mountSegment = segment => {
   return segmentWrap
 }
 
-const renderLine = (line, container) => {
-  var segments = line.map(segment => mountSegment(segment))
+const mountLine = (lineDefinition) => {
+  var segments = lineDefinition.map(segmentDefinition => mountSegment(segmentDefinition))
   var lineWrap = document.createElement('span')
   segments.forEach(segment => lineWrap.appendChild(segment))
 
-  container.appendChild(lineWrap)
+  return lineWrap
 }
 
-const render = (lines, container) => {
-  lines.forEach(line => renderLine(line, container))
-  $(container).bigtext({ maxfontsize: 10000 });
+const render = (lineDefinitions, container) => {
+  var lines = lineDefinitions.map(lineDefinition => mountLine(lineDefinition))
+  lines.forEach(line => container.appendChild(line))
+  
+  $(container).bigtext({ maxfontsize: Infinity })
 }
 
-const offBottom = element => {
+const isOffBottom = element => {
   return element.getBoundingClientRect().bottom > window.innerHeight
 }
 
-const unveilNext = () => {
-  let next = document.querySelectorAll('.segment:not(.unveiled)')[0]
-  if(next) { 
-    next.classList.add('unveiled')
-    if(offBottom(next)) {
-      window.scrollBy({ top: next.offsetHeight, behaviour: 'smooth' })
-    }
+const unveilNextAndScroll = () => {
+  var next = document.querySelectorAll('.segment:not(.unveiled)')[0]
+  if (!next) return
+  
+  next.classList.add('unveiled')
+  if (isOffBottom(next)) {
+    window.scrollBy({ top: next.parentElement.offsetHeight, behaviour: 'smooth' })
   }
 }
 
 const setupListeners = () => {
-  document.addEventListener('click', unveilNext)
+  document.addEventListener('click', unveilNextAndScroll)
 }
 
 const disableSelection = target => {
@@ -67,6 +79,6 @@ const disableSelection = target => {
   target.onmousedown = () => { return false }
 }
 
-render(LINES, document.getElementById('container'))
+render(LINE_DEFINITIONS, document.getElementById('container'))
 setupListeners()
 disableSelection(document.body)
