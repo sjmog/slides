fbcdc2268fb907ce35b76e7959d8f80bf8600f51
Sam Morgan <s_morgan@me.com>
2018-11-29 18:12:51 +0000

    Add talk on learning

--- src/index.js
+++ src/index.js
@@ -1,52 +1,82 @@
 require('smoothscroll-polyfill').polyfill()
 
 const LINE_DEFINITIONS = [
-  ["Amazon Alexa"], // is a lot of fun
-  ["Ruby"],  // is a lot of fun, too. So
-  ["Why can't they just be friends"], // ? Well, all great friendships can turn sour.
-  ["Annoyance #1"],                   // is that
-  ["AWS Lambda"],                     // doesn't support Ruby, and nor does it intend to. This is a
-  ["Pain in the ass"],                // especially because Lambda is all about little web projects, and
-  ["Ruby ♥ little web projects"],     // . I mean, it's got wonderful things like
+  ["Learning"],
+  ["  Six key ways to build it in  "],
   [
-    ["Rack"],         // and a minimal implementation, which is
-    ["(Sinatra)"]     // which provides lovely stuff for small web projects, like a
-  ],          
-  [
-    ["Readable DSL"], //
-    ["Easy Routing"], // , and being really
-    ["Flexible"]      // to all the weird shit you might want to do with it. So the thing is
-  ],
-  ["Alexa + Sinatra?"],            // should feel like pretty good companions. It's weird that Lambda is standing in their way when
-  ["They could be good friends"],  // . But there's another
-  ["Annoyance #2"],                // , and that's
-  ["JSON"],                        // . JSON in Ruby means lots of
-  [
-    ["Disgusting hash conversions"], // and a bunch of
-    ["Painful testing"]              // with fixture files full of requests and responses all over.
-  ],
-  ["Disgusting"],                    // . Ugh. So then we get to 
-  ["Annoyance #3"],                  // , which is
-  ["HTTPS Endpoints"],               // . Alexa needs 'em, and deploying a Ruby app over and over with every file change is a pain. An easy way out is
-  [
-    ["ngrok"],           // , a tunnelling program that comes with
-    ["HTTPS"],           // and paying for it even gives you a
-    ["Custom domain"]    // , so you don't have to change it in your skill config each time you restart your computer. So how about a
+    ["1. Context"],
+    ["2. Goals"]
   ],
   [
-    ["Ruby + Alexa framework"],      //
-    ["(for Sinatra)"],               //
-    ["(ngrok-compatible)"]           // ? Well, here's one I made earlier. I call it
+    ["3. Tasks"],
+    ["4. Resources"]
   ],
-  ["Ralyxa"],                        // . Kinda like 'Ruby + Alexa'. It's got a reasonably
   [
-    ["Nice interface"],              // with lots of nice normal
-    ["Sensible defaults"],           // if you don't like coding much, and it's
-    ["Sinatra-esque"]                // in that it's all about "quickly creating Alexa skills in Ruby with minimal effort". Built-in is the
+    ["5. Assessment"],
+    ["6. Feedback"]
   ],
-  ["ask and tell syntax"],                    // for natural interactions, and you get
-  ["Separate routing from Intent handling"],  // allowing you to pop it into an existing Sinatra application. And with that, let's do a
-  ["Demo"]
+  ["   Context   "],
+  ["       Start here       "],
+  ["Stop demonstrating downwards"],
+  ["Start mapping upwards"],
+  [
+    ["How does my system fit other systems?"],
+    ["Where am I doing my work right now?"]
+  ],
+  [
+    ["Diagramming sessions"],
+    ["Constant links to the domain"]
+  ],
+  ["   Goals   "],
+  ["(A theory of goal setting & task performance – Locke)"],
+  ["What can I say about myself?"],
+  [
+    ["Specific"],
+    ["Aspirational"],
+    ["Expected"],
+    ["Believed"]
+  ],
+  ["(Pygmalion in the classroom – Rosenthal and Jacobson)"],
+  ["   Tasks   "],
+  ["   Everything is an exercise   "],
+  [
+    ["Time"],
+    ["Minimal unblocking"],
+    ["Keywords"],
+    ["Encouragement"]
+  ],
+  ["   Resources   "],
+  [
+    ["You"],
+    ["Reading"],
+    ["Prep exercises"]
+  ],
+  ["      Be Socratic      "],
+  [
+    ["Ask questions"],
+    ["Build a library"],
+    ["Keep focussed"]
+  ],
+  ["  ☠️ ☠️ ☠️ Avoid Tutorials ☠️ ☠️ ☠️  "],
+  ["   Assessment   "],
+  [
+    ["Concretes"],
+    ["Concepts"],
+    ["Skills"],
+    ["Behaviours"]
+  ],
+  [
+    ["Concepts – Ask"],
+    ["Skills – Observe"]
+  ],
+  ["   Feedback   "],
+  ["   Have a structure   "],
+  ["Build it in with authentic tools"],
+  [
+    ["Require intentionality"],
+    ["Keep it tight"],
+    ["Demand iteration"]
+  ]
 ]
 
 const mountSegment = segment => {
